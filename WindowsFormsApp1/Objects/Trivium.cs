a6b8962aeec1002c2d1a84b4264130fd173aabb2
USER <USER@DESKTOP-5G70Q17>
2019-07-07 01:08:39 -0300

    saco comentarios

--- WindowsFormsApp1/Objects/Trivium.cs
+++ WindowsFormsApp1/Objects/Trivium.cs
@@ -24,21 +24,7 @@ namespace WindowsFormsApp1.Objects
             this.InitializeInternalState(new BitArray(encoding.GetBytes(initialVector)));
             return this.GenerateKeyStream(fileSizeInBits);
         }
-        /*
-        private void UpdateState()
-        {
-            t1 = (t1 | internalState[90] & internalState[91] | internalState[170]);
-            t2 = (t2 | internalState[174] & internalState[175] | internalState[263]);
-            t3 = (t3 | internalState[285] & internalState[286] | internalState[68]);
-
-            for (int i = 91; i >= 0; i--) internalState[i + 1] = internalState[i];
-            internalState[0] = t3;
-            for (int i = 175; i >= 93; i--) internalState[i + 1] = internalState[i];
-            internalState[93] = t1;
-            for (int i = 286; i >= 177; i--) internalState[i + 1] = internalState[i];
-            internalState[177] = t2;
-        }*/
-
+        
         private void UpdateState()
         {
             t1 = (t1 | internalState.Get(90) & internalState.Get(91) | internalState.Get(170));
@@ -52,24 +38,6 @@ namespace WindowsFormsApp1.Objects
             for (int i = 286; i >= 177; i--) internalState.Set((i + 1),internalState.Get(i));
             internalState.Set(177, t2);
         }
-
-        /*private void InitializeInternalState(BitArray initialVector)
-        {
-            for (int i = 0; i < 80; i++) internalState[i] = key[i];
-            for (int i = 80; i < 93; i++) internalState[i] = false;
-            for (int i = 0; i < 80; i++) internalState[93+i] = initialVector[i];
-            for (int i = 173; i < 285; i++) internalState[i] = false;
-            internalState[285] = internalState[286] = internalState[287] = true;
-
-            //update state for 4*288 rounds
-            for(int i=0;i<4*288;i++)
-            {
-                t1 = (internalState[65] | internalState[92]);
-                t2 = (internalState[161] | internalState[176]);
-                t3 = (internalState[242] | internalState[287]);
-                this.UpdateState();
-            }
-        }*/
 
         private void InitializeInternalState(BitArray initialVector)
         {
@@ -90,21 +58,6 @@ namespace WindowsFormsApp1.Objects
                 this.UpdateState();
             }
         }
-
-        /*
-        private BitArray GenerateKeyStream(int fileSize)
-        {
-            BitArray keyStream = new BitArray(fileSize);
-            for (int i = 0; i < fileSize; i++)
-            {
-                t1 = (internalState[65] | internalState[92]);
-                t2 = (internalState[161] | internalState[176]);
-                t3 = (internalState[242] | internalState[287]);
-                keyStream[i] = (t1 | t2 | t3);
-                this.UpdateState();
-            }
-            return keyStream;
-        }*/
 
         private BitArray GenerateKeyStream(int fileSize)
         {
